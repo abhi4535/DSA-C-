310126ac95e785cb97ef290215d1f02a460b5869
abhishek yadav <georgeabhi4535@gmail.com>
2025-02-12 16:16:22 +0530

    pairing friend problem

---
+++ DP/friend_pairing.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dp;
+
+int f(int n){
+    if(n==0 ) return 1;
+
+    if(dp[n]!=-1) return dp[n];
+
+    return dp[n]= f(n-1)+(n-1)*f(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    dp.clear();
+    dp.resize(n+1, -1);
+    cout<<f(n)<<endl;
+
+    return 0;
+
+}
